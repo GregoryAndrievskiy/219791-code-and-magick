924e12806e5cd30dbeec8600aa8a3547cf8246dc
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-04-22 14:40:00 +0400

    исправил colorize, debounce

--- js/trash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.debounce = (function (func, delay) {
-  var lastTimeout;
-  if (lastTimeout) {
-    window.clearTimeout(lastTimeout);
-  }
-  lastTimeout = window.setTimeout(func, delay);
-});
